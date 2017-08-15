00dc5b5c71a47d7f1a5b6f58e63b919932c57411
Daniel Hernandez <daniel56hdz@gmail.com>
2017-08-15 17:46:58 -0500

    set up twitter keys

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'CEt0C8bsyQoTm09kzy6zbfm5Y',
+  consumer_secret: 'qjnxCfZTlN9TXtpJjb1oOmXo7apc1hCXZXMhaNlznRnvHK3bMr',
+  access_token_key: '888519949328764930-wA1cdtwxCWrXqlOghaW26OEdLPdNaWf',
+  access_token_secret: 'vFrVG52z6F60RSh3BTnVNsZEhpwyVGOyEA7iLjhAErbQZ',
+}
