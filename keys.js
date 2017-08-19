4cebc738fe9172cd9f517a096f3358376f3366a4
Daniel Hernandez <daniel56hdz@gmail.com>
2017-08-19 00:54:35 -0500

    added spotify lookup functionality

--- keys.js
+++ keys.js
@@ -5,4 +5,9 @@ exports.twitterKeys = {
   consumer_secret: 'qjnxCfZTlN9TXtpJjb1oOmXo7apc1hCXZXMhaNlznRnvHK3bMr',
   access_token_key: '888519949328764930-wA1cdtwxCWrXqlOghaW26OEdLPdNaWf',
   access_token_secret: 'vFrVG52z6F60RSh3BTnVNsZEhpwyVGOyEA7iLjhAErbQZ',
-}
+};
+
+exports.spotifyKeys = {
+  id: '13c8eea72bec4c1d9f948866a9236932',
+  secret: '3aea4f65b4844344a5974c25063e1d84'
+};
--- liri.js
+++ liri.js
@@ -3,13 +3,16 @@ const yargs = require('yargs');
 const _ = require('lodash');
 const axios = require('axios');
 const Twitter = require('twitter');
-const spotify = require('spotify');
+const Spotify = require('node-spotify-api');
+
+const http = require("http");
 
 // Packages made inhouse
 const keys = require('./keys.js');
 
 // User based authentication
 var client = new Twitter(keys.twitterKeys);
+var spotify = new Spotify(keys.spotifyKeys);
 
 // Configuring yargs to display information about commands when typing the help flag
 const argv = yargs
@@ -22,6 +25,8 @@ const argv = yargs
 
 // Retrieving user input from terminal
 var command = argv._[0];
+var query = argv._[1];
+
 
 //Logic for commands
 if (command === 'my-tweets') {
@@ -39,8 +44,25 @@ if (command === 'my-tweets') {
             })
         }
     });
-} else if(command === 'spotify-this-song'){
+} else if (command === 'spotify-this-song') {
     console.log('Searching Through Spotify');
     console.log('-------------------');
-    
+
+    spotify.search({
+        type: 'track',
+        query,
+        limit: 5
+    }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        var spotifyObj = data.tracks.items;
+        spotifyObj.forEach((song) => { // Ternary operators used for edge cases where property might be null/undefined
+            console.log(`Artist: ${song.artists[0].name ? song.artists[0].name : 'Not Available'}`);
+            console.log(`Song: ${song.name ? song.name : 'Not Available'}`);
+            console.log(`Preview link: ${song.preview_url ? song.preview_url : 'Not Available'}`);
+            console.log(`Album name: ${song.album.name ? song.album.name : 'Not Available'}`)
+            console.log('--------------')
+        });
+    });
 }
